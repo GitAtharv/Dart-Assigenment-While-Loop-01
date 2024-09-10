6ae44d6b0e37faa815be585cd2e08b1200b466b8
GitAtharv <163186776+GitAtharv@users.noreply.github.com>
2024-09-10 20:25:23 +0530

    Add files via upload

---
+++ program9.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  int numDays=7;
+   while (numDays>=0) {
+    if (numDays==0) {
+      print("0 days Assigenment is overdue");
+    }
+    else{
+      print("$numDays days remaining");
+    }
+     numDays--;
+   }
+  }
